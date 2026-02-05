HSK1873/LuxonServer-Testing

Initial public commit

include/luxon/server/game.hpp
@@ -1,0 +1,138 @@
+#pragma once
+
+#include "global.hpp"
+#include "apps.hpp"
+#include "lobby.hpp"
+#include "peer.hpp"
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+#include "game_plugin_base.hpp"
+#endif
+
+#include <memory>
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <variant>
+#include <luxon/photon_types.hpp>
+#include <luxon/enet_peer.hpp>
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+#define GAME_PLUGINS_INVOKE(...)                                                                                                                               \
+    {                                                                                                                                                          \
+        using namespace game_plugins;                                                                                                                          \
+        __VA_ARGS__                                                                                                                                            \
+    }
+#else
+#define GAME_PLUGINS_INVOKE(...)
+#endif
+
+namespace server {
+class App;
+struct Lobby;
+struct Peer;
+
+struct GamePeer {
+    std::weak_ptr<Peer> peer;
+    int32_t actor_id{};
+    photon::Hashtable actor_props;
+    std::unordered_set<uint8_t> interest_groups;
+
+    bool is_valid() const { return actor_id > 0; }
+    bool has_interest_group(uint8_t group) const;
+    bool disconnect();
+};
+
+struct Event {
+    uint8_t code;
+    int32_t sender_actor_id;
+    std::variant<std::monostate, uint8_t, std::unordered_set<int32_t>> receivers{};
+    uint8_t interest_group{};
+    enet::EnetDeliveryMode delivery_mode = enet::EnetDeliveryMode::Reliable;
+    photon::Value data;
+    photon::Dictionary top_params;
+    photon::ByteArray serialization_cache;
+
+    photon::Hashtable& make_params_hashtable() { return *(data = std::make_shared<photon::Hashtable>()).get<photon::HashtablePtr>(); }
+    photon::Hashtable *get_params_hashtable() {
+        if (data.is<photon::HashtablePtr>())
+            return data.get<photon::HashtablePtr>().get();
+        return nullptr;
+    }
+};
+
+struct Game : std::enable_shared_from_this<Game> {
+    const std::shared_ptr<App> app;
+    Lobby& lobby;
+    const std::string id;
+
+    ~Game();
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+    std::vector<std::unique_ptr<game_plugins::PluginBase>> plugins;
+#endif
+
+    uint8_t flags = 3; // CheckUserOnJoin | DeleteCacheOnLeave
+    bool is_open = true;
+    bool is_visible = true;
+    int32_t player_ttl = 0;
+    int32_t empty_game_ttl = 0;
+    uint8_t max_peers = 0;
+    int32_t master_actor = 1;
+    int32_t last_actor_id = 0;
+    std::unordered_set<std::string> expected_users;
+    photon::Hashtable custom_props;
+    std::vector<std::string> lobby_props;
+    std::list<Event> event_cache;
+
+    Game(std::shared_ptr<App> app, Lobby& lobby, std::string id) : app(std::move(app)), lobby(lobby), id(std::move(id)) {}
+
+    std::list<GamePeer> peers;
+
+    GamePeer create_peer(std::shared_ptr<Peer> peer);
+    GamePeer *add_peer(GamePeer&& game_peer);
+    bool remove_peer(const std::shared_ptr<Peer>& peer);
+    bool flood_peer(GamePeer *game_peer);
+    GamePeer *find_peer(int32_t actor_id);
+    void broadcast_event(Event& event);
+    int16_t validate_join(const std::string& user_id, size_t new_expected_users_count = 0) const;
+
+    void trigger_lobby_update();
+
+    photon::Value get_game_prop(const photon::Value& key);
+    photon::Hashtable get_basic_game_props();
+    photon::Hashtable get_game_props(bool no_custom = false);
+    photon::Hashtable get_actor_props();
+    void insert_game_props(photon::Hashtable update);
+    bool expect_game_props(photon::Hashtable expected);
+    void insert_actor_props(int32_t actor_id, const photon::Hashtable& update);
+    bool expect_actor_props(int32_t actor_id, const photon::Hashtable& expected);
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+    template <typename InfoStruct>
+    game_plugins::Result execute_plugin_chain(game_plugins::Result (game_plugins::PluginBase::*method)(luxon::photon::OperationRequest&, InfoStruct&),
+                                              luxon::photon::OperationRequest& req, InfoStruct& info) {
+        for (const auto& plugin : plugins) {
+            game_plugins::Result result = ((*plugin).*method)(req, info);
+            if (result != game_plugins::Result::Continue)
+                return result;
+        }
+
+        return game_plugins::Result::Continue;
+    }
+
+    template <typename InfoStruct>
+    game_plugins::Result execute_plugin_chain(game_plugins::Result (game_plugins::PluginBase::*method)(InfoStruct&), InfoStruct& info) {
+        for (const auto& plugin : plugins) {
+            game_plugins::Result result = ((*plugin).*method)(info);
+            if (result != game_plugins::Result::Continue)
+                return result;
+        }
+
+        return game_plugins::Result::Continue;
+    }
+#endif
+
+    // Helper to check if event data matches a filter hashtable
+    static bool matches_filter(const photon::Value& event_data, const photon::Hashtable& filter);
+};
+} // namespace server

include/luxon/server/peer.hpp
@@ -1,0 +1,25 @@
+#pragma once
+
+#include "global.hpp"
+#include "logger.hpp"
+#include "peer_persistence.hpp"
+
+#include <memory>
+#include <luxon/enet_peer.hpp>
+#include <luxon/photon_protocol.hpp>
+#include <luxon/photon_crypto.hpp>
+
+namespace server {
+struct PeerPersistent;
+
+struct Peer {
+    std::shared_ptr<enet::EnetPeer> enet_peer;
+    std::shared_ptr<logger> log;
+    std::unique_ptr<photon::ICryptoProvider> crypto;
+    std::unique_ptr<PeerPersistent> persistent;
+
+    bool is_authenticated() const { return persistent != nullptr; }
+    void send(const photon::ByteArray& payload, const enet::EnetSendOptions& opt);
+    void disconnect();
+};
+} // namespace server

src/authentication.cpp
@@ -1,0 +1,72 @@
+#include "authentication.hpp"
+#include "global.hpp"
+#include "codes.hpp"
+#include "peer.hpp"
+#include "peer_persistence.hpp"
+#include "apps.hpp"
+
+#include <algorithm>
+#include <random>
+#include <luxon/photon_protocol.hpp>
+
+namespace server {
+namespace {
+std::string generate_user_id() {
+    static std::mt19937 gen{std::random_device{}()};
+    const std::string_view charset = "0123456789ABCDEF";
+    std::uniform_int_distribution<size_t> dist(0, charset.size() - 1);
+
+    std::string fres(32, '\0');
+    std::ranges::generate(fres, [&] { return charset[dist(gen)]; });
+    return fres;
+}
+} // namespace
+
+photon::OperationResponse authenticate(ServerManager& server_manager, Peer& peer, const photon::OperationRequest& req, bool is_encrypted,
+                                       const enet::EnetCommandHeader& cmd_header, bool refresh_token) {
+    // Decide on algorithm
+    const bool token_auth = req.parameters.contains(DictKeyCodes::LoadBalancing::Token);
+
+    // Try authentication
+    try {
+        if (token_auth) {
+            // Token mechanism
+            peer.persistent = load_persistent_peer(server_manager, req.parameters.at(DictKeyCodes::LoadBalancing::Token).get<std::string>(), refresh_token);
+        } else {
+            // Regular mechanism (very simple for now)
+            auto& p = peer.persistent = create_persistent_peer();
+            p->app = App::get(server_manager, req.parameters.at(DictKeyCodes::LoadBalancing::ApplicationId).get<std::string>(),
+                              req.parameters.at(DictKeyCodes::LoadBalancing::AppVersion).get<std::string>());
+            if (req.parameters.contains(DictKeyCodes::LoadBalancing::UserId))
+                p->user_id = req.parameters.at(DictKeyCodes::LoadBalancing::UserId).get<std::string>();
+            else
+                p->user_id = generate_user_id();
+        }
+    } catch (const std::out_of_range& e) {
+        // Handle bad parameter map access
+        return {.operation_code = req.operation_code,
+                .return_code = ErrorCodes::Data::InvalidRequestParameters,
+                .debug_message = std::format("Missing parameter: {}", e.what())};
+    } catch (const std::bad_variant_access& e) {
+        // Handle bad parameter variant access
+        return {.operation_code = req.operation_code,
+                .return_code = ErrorCodes::Data::InvalidRequestParameters,
+                .debug_message = std::format("Invalid parameter type: {}", e.what())};
+    }
+
+    // Check for success
+    if (!peer.persistent) {
+        // Handle authentication failure
+        return {.operation_code = req.operation_code,
+                .return_code = token_auth ? ErrorCodes::Auth::AuthenticationTokenExpired : ErrorCodes::Auth::InvalidAuthentication,
+                .debug_message = "Authentication failure: Got no persistent peer data"};
+    }
+
+    peer.log->info("Client has authenticated as: {}", peer.persistent->user_id);
+
+    photon::OperationResponse resp{.operation_code = req.operation_code};
+    if (refresh_token)
+        resp.parameters[DictKeyCodes::LoadBalancing::Token] = peer.persistent->token;
+    return resp;
+}
+} // namespace server

BTFighter/LuxonServer

include/luxon/server/game.hpp
@@ -1,0 +1,138 @@
+#pragma once
+
+#include "global.hpp"
+#include "apps.hpp"
+#include "lobby.hpp"
+#include "peer.hpp"
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+#include "game_plugin_base.hpp"
+#endif
+
+#include <memory>
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <variant>
+#include <luxon/ser_types.hpp>
+#include <luxon/enet_peer.hpp>
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+#define GAME_PLUGINS_INVOKE(...)                                                                                                                               \
+    {                                                                                                                                                          \
+        using namespace game_plugins;                                                                                                                          \
+        __VA_ARGS__                                                                                                                                            \
+    }
+#else
+#define GAME_PLUGINS_INVOKE(...)
+#endif
+
+namespace server {
+class App;
+struct Lobby;
+struct Peer;
+
+struct GamePeer {
+    std::weak_ptr<Peer> peer;
+    int32_t actor_id{};
+    ser::Hashtable actor_props;
+    std::unordered_set<uint8_t> interest_groups;
+
+    bool is_valid() const { return actor_id > 0; }
+    bool has_interest_group(uint8_t group) const;
+    bool disconnect();
+};
+
+struct Event {
+    uint8_t code;
+    int32_t sender_actor_id;
+    enet::EnetDeliveryMode delivery_mode = enet::EnetDeliveryMode::Reliable;
+    uint8_t channel{};
+    std::variant<std::monostate, uint8_t, std::unordered_set<int32_t>> receivers{};
+    uint8_t interest_group{};
+    ser::Value data;
+    ser::Dictionary top_params;
+
+    ser::Hashtable& make_params_hashtable() { return *(data = std::make_shared<ser::Hashtable>()).get<ser::HashtablePtr>(); }
+    ser::Hashtable *get_params_hashtable() {
+        if (data.is<ser::HashtablePtr>())
+            return data.get<ser::HashtablePtr>().get();
+        return nullptr;
+    }
+};
+
+struct Game : std::enable_shared_from_this<Game> {
+    const std::shared_ptr<App> app;
+    Lobby& lobby;
+    const std::string id;
+
+    ~Game();
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+    std::vector<std::unique_ptr<game_plugins::PluginBase>> plugins;
+#endif
+
+    uint8_t flags = 3; // CheckUserOnJoin | DeleteCacheOnLeave
+    bool is_open = true;
+    bool is_visible = true;
+    int32_t player_ttl = 0;
+    int32_t empty_game_ttl = 0;
+    uint8_t max_peers = 0;
+    int32_t master_actor = 1;
+    int32_t last_actor_id = 0;
+    std::unordered_set<std::string> expected_users;
+    ser::Hashtable custom_props;
+    std::vector<std::string> lobby_props;
+    std::list<Event> event_cache;
+
+    Game(std::shared_ptr<App> app, Lobby& lobby, std::string id) : app(std::move(app)), lobby(lobby), id(std::move(id)) {}
+
+    std::list<GamePeer> peers;
+
+    GamePeer create_peer(std::shared_ptr<Peer> peer);
+    GamePeer *add_peer(GamePeer&& game_peer);
+    bool remove_peer(const std::shared_ptr<Peer>& peer);
+    bool flood_peer(GamePeer *game_peer);
+    GamePeer *find_peer(int32_t actor_id);
+    void broadcast_event(Event& event);
+    int16_t validate_join(const std::string& user_id, size_t new_expected_users_count = 0) const;
+
+    void trigger_lobby_update();
+
+    ser::Value get_game_prop(const ser::Value& key);
+    ser::Hashtable get_basic_game_props();
+    ser::Hashtable get_game_props(bool no_custom = false);
+    ser::Hashtable get_actor_props();
+    void insert_game_props(ser::Hashtable update);
+    bool expect_game_props(ser::Hashtable expected);
+    void insert_actor_props(int32_t actor_id, const ser::Hashtable& update);
+    bool expect_actor_props(int32_t actor_id, const ser::Hashtable& expected);
+
+#ifdef LUXON_SERVER_ENABLE_PLUGINS
+    template <typename InfoStruct>
+    game_plugins::Result execute_plugin_chain(game_plugins::Result (game_plugins::PluginBase::*method)(luxon::ser::OperationRequestMessage&, InfoStruct&),
+                                              luxon::ser::OperationRequestMessage& req, InfoStruct& info) {
+        for (const auto& plugin : plugins) {
+            game_plugins::Result result = ((*plugin).*method)(req, info);
+            if (result != game_plugins::Result::Continue)
+                return result;
+        }
+
+        return game_plugins::Result::Continue;
+    }
+
+    template <typename InfoStruct>
+    game_plugins::Result execute_plugin_chain(game_plugins::Result (game_plugins::PluginBase::*method)(InfoStruct&), InfoStruct& info) {
+        for (const auto& plugin : plugins) {
+            game_plugins::Result result = ((*plugin).*method)(info);
+            if (result != game_plugins::Result::Continue)
+                return result;
+        }
+
+        return game_plugins::Result::Continue;
+    }
+#endif
+
+    // Helper to check if event data matches a filter hashtable
+    static bool matches_filter(const ser::Value& event_data, const ser::Hashtable& filter);
+};
+} // namespace server

include/luxon/server/peer.hpp
@@ -1,0 +1,25 @@
+#pragma once
+
+#include "global.hpp"
+#include "logger.hpp"
+#include "peer_persistence.hpp"
+
+#include <memory>
+#include <luxon/enet_peer.hpp>
+#include <luxon/ser_interface.hpp>
+#include <luxon/ser_encryption.hpp>
+
+namespace server {
+struct PeerPersistent;
+
+struct Peer {
+    std::unique_ptr<ser::IProtocol> protocol;
+    std::shared_ptr<enet::EnetPeer> enet_peer;
+    std::shared_ptr<logger> log;
+    std::unique_ptr<PeerPersistent> persistent;
+
+    bool is_authenticated() const { return persistent != nullptr; }
+    void send(const ser::ByteArray& payload, const enet::EnetSendOptions& opt);
+    void disconnect();
+};
+} // namespace server

src/authentication.cpp
@@ -1,0 +1,72 @@
+#include "authentication.hpp"
+#include "global.hpp"
+#include "codes.hpp"
+#include "peer.hpp"
+#include "peer_persistence.hpp"
+#include "apps.hpp"
+
+#include <algorithm>
+#include <random>
+#include <luxon/ser_interface.hpp>
+
+namespace server {
+namespace {
+std::string generate_user_id() {
+    static std::mt19937 gen{std::random_device{}()};
+    const std::string_view charset = "0123456789ABCDEF";
+    std::uniform_int_distribution<size_t> dist(0, charset.size() - 1);
+
+    std::string fres(32, '\0');
+    std::ranges::generate(fres, [&] { return charset[dist(gen)]; });
+    return fres;
+}
+} // namespace
+
+ser::OperationResponseMessage authenticate(ServerManager& server_manager, Peer& peer, const ser::OperationRequestMessage& req,
+                                       const enet::EnetCommandHeader& cmd_header, bool refresh_token) {
+    // Decide on algorithm
+    const bool token_auth = req.parameters.contains(DictKeyCodes::LoadBalancing::Token);
+
+    // Try authentication
+    try {
+        if (token_auth) {
+            // Token mechanism
+            peer.persistent = load_persistent_peer(server_manager, req.parameters.at(DictKeyCodes::LoadBalancing::Token).get<std::string>(), refresh_token);
+        } else {
+            // Regular mechanism (very simple for now)
+            auto& p = peer.persistent = create_persistent_peer();
+            p->app = App::get(server_manager, req.parameters.at(DictKeyCodes::LoadBalancing::ApplicationId).get<std::string>(),
+                              req.parameters.at(DictKeyCodes::LoadBalancing::AppVersion).get<std::string>());
+            if (req.parameters.contains(DictKeyCodes::LoadBalancing::UserId))
+                p->user_id = req.parameters.at(DictKeyCodes::LoadBalancing::UserId).get<std::string>();
+            else
+                p->user_id = generate_user_id();
+        }
+    } catch (const std::out_of_range& e) {
+        // Handle bad parameter map access
+        return {.operation_code = req.operation_code,
+                .return_code = ErrorCodes::Data::InvalidRequestParameters,
+                .debug_message = std::format("Missing parameter: {}", e.what())};
+    } catch (const std::bad_variant_access& e) {
+        // Handle bad parameter variant access
+        return {.operation_code = req.operation_code,
+                .return_code = ErrorCodes::Data::InvalidRequestParameters,
+                .debug_message = std::format("Invalid parameter type: {}", e.what())};
+    }
+
+    // Check for success
+    if (!peer.persistent) {
+        // Handle authentication failure
+        return {.operation_code = req.operation_code,
+                .return_code = token_auth ? ErrorCodes::Auth::AuthenticationTokenExpired : ErrorCodes::Auth::InvalidAuthentication,
+                .debug_message = "Authentication failure: Got no persistent peer data"};
+    }
+
+    peer.log->info("Client has authenticated as: {}", peer.persistent->user_id);
+
+    ser::OperationResponseMessage resp{.operation_code = req.operation_code};
+    if (refresh_token)
+        resp.parameters[DictKeyCodes::LoadBalancing::Token] = peer.persistent->token;
+    return resp;
+}
+} // namespace server