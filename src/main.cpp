a3c40961166989c153ad73c4ee1949f375008989
niansa <tuxifan@posteo.de>
2026-02-18 20:21:56 +0100

    Added generic platform support

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 #include "luxon/server/server_manager.hpp"
 #include "platform.hpp"
 
-#include <optional>
-#include <csignal>
-
 int main() {
     Platform P;
     server::ServerManager("config.yml").run();
